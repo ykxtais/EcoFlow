9a4834c8b335eacf7fcfafacbf0b9cda24be4218
irissuu <ykxiris@gmail.com>
2024-11-14 17:35:19 -0300

    not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="error">
+          <h1>404 - Página não encontrada</h1>
+          <p>Desculpe, a página que você está procurando não existe.</p>
+      </div>
+    )
+  }
