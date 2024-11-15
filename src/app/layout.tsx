858c74ca91e2447452e396dcab04131cfa6c90d9
irissuu <ykxiris@gmail.com>
2024-11-15 20:05:03 -0300

    menu e adicionando eve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "NovaApex",
+  title: "EcoFlow",
   description: "Equipe desenvolvedora responsável por EcoFlow",
 };
 
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,11 +4,10 @@ export default function Menu() {
     return (
         <nav>
             <ul>
-                <li> <Link href="/">Home</Link> </li>
                 <li> <Link href="/informa">Energias</Link> </li>
                 <li> <Link href="/equipe">Equipe</Link> </li>
                 <li> <Link href="/suporte">Suporte</Link> </li>
-                <li> <Link href="/auth">Login</Link> </li>
+                <li> <Link href="/chatboteve">Eve</Link> </li>
             </ul>
         </nav>
     );
