0d24a4ff9fd0ac18a5eed1de865dd36c7bea6102
irissuu <ykxiris@gmail.com>
2024-11-19 02:33:54 -0300

    simbolos

--- ecoflow/src/components/Menu/Menu.tsx
+++ ecoflow/src/components/Menu/Menu.tsx
@@ -4,11 +4,11 @@ export default function Menu() {
     return (
         <nav>
             <ul>
-                <li> <Link href="/">Home </Link> </li>
-                <li> <Link href="/informa">Energias </Link> </li>
-                <li> <Link href="/equipe">Equipe </Link> </li>
-                <li> <Link href="/suporte">Suporte </Link> </li>
-                <li> <Link href="/chatboteve">Eve </Link> </li>
+                <li> <Link href="/">Home ⌂</Link> </li>
+                <li> <Link href="/informa">Energias ⓘ</Link> </li>
+                <li> <Link href="/equipe">Equipe ✉︎</Link> </li>
+                <li> <Link href="/suporte">Suporte ✆</Link> </li>
+                <li> <Link href="/chatboteve">Eve ✧</Link> </li>
             </ul>
         </nav>
     );
